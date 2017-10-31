adc7864eb0ae3aa955b6744f67e8dddc2a42d1ce
Tjay <Kiox1995@gmail.com>
2017-10-31 21:30:02 +0100

    create position class

--- src/main/java/objectives/Position.java
+++ src/main/java/objectives/Position.java
@@ -2,19 +2,52 @@ package objectives;
 
 public class Position {
 
-    private int x;
-    private int y;
+    private int x = 0;
+    private int y = 0;
 
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @return den x value of the position
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * @return the y value of the position
+     */
     public int getY() {
         return y;
+    }
+
+    /**
+     * @param x the new x value of the position
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @param y the new y value of the position
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Position){
+            Position pos2 = (Position)obj;
+            return
+                    (pos2.getX() == this.getX()) &&
+                            (pos2.getY() == this.getY());
+
+        } else {
+            return false;
+        }
     }
 }
