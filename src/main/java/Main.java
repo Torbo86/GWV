5502b370c1d9919c42a90fdca8801b0e17c8f412
Tjay <Kiox1995@gmail.com>
2017-10-31 22:36:43 +0100

    add reader and add target as ignored folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import Services.LabyrinthFileReader;
+
+import java.io.File;
+
 public class Main {
 
+    public final static String FIRST_LABYRINTH = "src/main/resources/labyrinths/blatt3_environment.txt";
+
     public static void main(String... args){
-        //Start something
+        LabyrinthFileReader reader = new LabyrinthFileReader();
+
+        reader.readLabyrinthFile(new File(FIRST_LABYRINTH));
     }
 }
--- src/main/java/Services/LabyrinthFileReader.java
+++ src/main/java/Services/LabyrinthFileReader.java
@@ -1,7 +1,33 @@
 package Services;
 
+import objectives.Labyrinth;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 /**
  * This class can read nats labyrinths
  */
 public class LabyrinthFileReader {
+
+    public Labyrinth readLabyrinthFile(File file) {
+        char playGround[][];
+
+        try {
+            List<String> stringList = Files.readAllLines(file.toPath());
+            playGround = new char[stringList.size()][];
+
+            for(int i = 0; i < stringList.size(); i++){
+                playGround[i] = stringList.get(i).toCharArray();
+            }
+
+            System.out.println("Test");
+        } catch (IOException e){
+            e.getStackTrace();
+
+        }
+        return null;
+    }
 }
