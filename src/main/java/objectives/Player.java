c2415b31c9677516ab17649f4feb3df8a8f92e20
Tjay <Kiox1995@gmail.com>
2017-10-31 21:39:40 +0100

    Create SearchState and Player class

--- src/main/java/objectives/Player.java
+++ src/main/java/objectives/Player.java
@@ -2,4 +2,10 @@ package objectives;
 
 public class Player {
 
+    private Position position;
+
+    public Player(Position position){
+        this.position = position;
+    }
+
 }
--- src/main/java/objectives/SearchStates.java
+++ src/main/java/objectives/SearchStates.java
@@ -2,6 +2,19 @@ package objectives;
 
 public class SearchStates {
 
-    //SpielerPosition
-    //Goal
+    private Position goalPosition;
+    private Position playerPosition;
+
+    public SearchStates(final Position goalPosition, final Position playerPosition){
+        this.goalPosition = goalPosition;
+        this.playerPosition = playerPosition;
+    }
+
+    @Override
+    public String toString() {
+        return new String("GoalX: " + goalPosition.getX() +
+                            ", GoalY: " + goalPosition.getY() +
+                            ", PlayerX: " + playerPosition.getX() +
+                            ", PlayerY: " + playerPosition.getY());
+    }
 }
