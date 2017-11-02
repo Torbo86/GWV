93d0efb14ef38682a6adc85a57797f4e0698082e
Tjay <Kiox1995@gmail.com>
2017-11-02 13:04:40 +0100

    make reader bettter and add constructor to labyrinth

--- src/main/java/Services/LabyrinthFileReader.java
+++ src/main/java/Services/LabyrinthFileReader.java
@@ -1,10 +1,14 @@
 package Services;
 
 import objectives.Labyrinth;
+import objectives.Player;
+import objectives.Position;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,8 +16,13 @@ import java.util.List;
  */
 public class LabyrinthFileReader {
 
+    /**
+     * This method reads the given file and creates a labyrinth
+     * @param file the file which should be read
+     * @return the labyrinth object which is in the file
+     */
     public Labyrinth readLabyrinthFile(File file) {
-        char playGround[][];
+        char playGround[][] = new char[1][1];
 
         try {
             List<String> stringList = Files.readAllLines(file.toPath());
@@ -28,6 +37,40 @@ public class LabyrinthFileReader {
             e.getStackTrace();
 
         }
-        return null;
+        Player player = new Player(getStartPosition(playGround));
+
+        return new Labyrinth(playGround, getGoalPosition(playGround), player);
+    }
+
+    private Position getGoalPosition(char[][] playGround) {
+        List<Position> goalPositionList = lookForSpecificCharInCharArray(playGround, 'G');
+        if(goalPositionList.size() == 1){
+            return goalPositionList.get(0);
+        } else {
+            throw new RuntimeException("There isnt 1 Goalposition, there are: "+ goalPositionList.size());
+        }
+    }
+
+    private Position getStartPosition(char[][] charArray){
+        List<Position> startPositionList = lookForSpecificCharInCharArray(charArray, 'S');
+        if(startPositionList.size() == 1){
+           return startPositionList.get(0);
+        } else {
+            throw new RuntimeException("There isnt 1 Startposition, there are: " + startPositionList.size());
+        }
+    }
+
+    private List<Position> lookForSpecificCharInCharArray(char[][] charArray, final char specificChar){
+        List<Position> positionList = new ArrayList<>();
+
+        for(int i = 0; i < charArray.length; i++){
+            for(int j = 0; j < charArray[i].length; j++){
+                if(charArray[i][j] == specificChar){
+                    positionList.add(new Position(i, j));
+                }
+            }
+        }
+
+        return positionList;
     }
 }
--- src/main/java/objectives/Labyrinth.java
+++ src/main/java/objectives/Labyrinth.java
@@ -2,9 +2,15 @@ package objectives;
 
 public class Labyrinth {
 
-    private int[] playground;
-    private int goal;
+    private char[][] playground;
+    private Position goal;
     private Player player;
+
+    public Labyrinth(final char[][] playground, final Position goal, final Player player){
+        this.playground = playground;
+        this.goal = goal;
+        this.player = player;
+    }
 
     // v(s -> (x,y), g -> (x,y)) -> aim -> v(g, s)
 }
