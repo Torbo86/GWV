64982cc5497bd12eb440bdea0e7b0fed8831a2b6
Tjay <Kiox1995@gmail.com>
2017-11-13 09:13:03 +0100

    Add Task 4.2.3 and 4.2.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import Services.BreadthFirstSearch;
 import Services.DepthFirstSearch;
 import Services.LabyrinthFileReader;
 import Services.SearchSpaceService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import objectives.Labyrinth;
 import objectives.internalRepresentation.Node;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,8 +33,22 @@ public class Main {
 
         List<Node> nodeList = depthFirstSearch.startDepthSearch();
 
-        if(nodeList.isEmpty()){
+        if (nodeList.isEmpty()) {
             System.out.println("Es wurde kein Weg gefunden");
         }
     }
 }
+
+// Aufgabe 4.2.3:
+/*
+    Wenn man mal das Labyrinth von Blatt3 benutzt, dann ist es möglich mit der Tiefensuche den selben Weg zu finden,
+    wie die Breitensuche (kürzester Weg). Nur wenn man die Prioritäten der Auswahl von den Weg ändert, dann kann man
+    auch zu einem viel längeren Weg kommen. Die Tiefensuche geht ja immer weiter vorwärts und findet dadurch den Weg
+    eventuell schneller, während die Breitensuche sich immer breite aufstellt mit jedem Durchgang, so aber den kürzesten
+    Weg am Ende bekommt.
+
+
+    Aufgabe 4.2.4:
+
+    Einfach mit SECOND_LABYRINTH in der Main-Klasse das Programm starten.
+ */
